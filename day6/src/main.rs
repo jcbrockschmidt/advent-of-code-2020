8664b23ac7ec1aed19ee5c33b1ec09b84ba6a7bb
Jaclyn Brockschmidt <jcbrockschmidt@gmail.com>
2020-12-06 18:20:28 -0800

    Add solution for day 6, part 2
    Solved in 11:07

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -9,8 +9,8 @@ fn usage(args: Vec<String>) {
     println!("Usage: {} input_file", args[0]);
 }
 
-/// Reads answers from a file and aggregates them by group.
-fn read_group_answers<P>(filename: P) -> Result<Vec<HashSet<char>>, String>
+/// Reads "yes" answers from a file and groups them.
+fn read_group_answers<P>(filename: P) -> Result<Vec<Vec<HashSet<char>>>, String>
 where
     P: AsRef<Path>,
 {
@@ -22,35 +22,51 @@ where
         Err(_) => return Err("Failed to read lines from file".into()),
     };
 
-    let mut answer_sets: Vec<HashSet<char>> = Vec::new();
-    let mut cur_set = HashSet::new();
+    let mut groups: Vec<Vec<HashSet<char>>> = Vec::new();
+    let mut cur_group: Vec<HashSet<char>> = Vec::new();
     for (i, line_res) in lines.enumerate() {
         let line = match line_res {
             Ok(l) => l,
             Err(_) => return Err(format!("Failed to read line {}", i + 1)),
         };
         if line.is_empty() {
-            answer_sets.push(cur_set);
-            cur_set = HashSet::new();
+            groups.push(cur_group);
+            cur_group = Vec::new();
         } else {
-            for ch in line.chars() {
-                cur_set.insert(ch);
-            }
+            let answers: HashSet<char> = line.chars().collect();
+            cur_group.push(answers);
         }
     }
-    if !cur_set.is_empty() {
-        answer_sets.push(cur_set);
+    if !cur_group.is_empty() {
+        groups.push(cur_group);
     }
-    Ok(answer_sets)
+    Ok(groups)
 }
 
-/// Counts the total number of answers over a collection of answer sets.
-fn count_yes_answers(answer_sets: &Vec<HashSet<char>>) -> usize {
-    let mut count = 0;
-    for answers in answer_sets {
-        count += answers.len();
+/// Sums total number of answers per-group where someone answered yes.
+fn sum_group_answers_union(group_answers: &Vec<Vec<HashSet<char>>>) -> usize {
+    let mut total = 0;
+    for group in group_answers {
+        let agg: HashSet<char> = group
+            .iter()
+            .fold(HashSet::new(), |acc, x| acc.union(&x).cloned().collect());
+        total += agg.len();
     }
-    count
+    total
+}
+
+/// Sums total number of answers per-group where everyone answered yes.
+fn sum_group_answers_inter(group_answers: &Vec<Vec<HashSet<char>>>) -> usize {
+    let mut total = 0;
+    for group in group_answers {
+        let first = group[0].iter().cloned().collect();
+        let agg: HashSet<char> = group
+            .iter()
+            .skip(1)
+            .fold(first, |acc, x| acc.intersection(&x).cloned().collect());
+        total += agg.len();
+    }
+    total
 }
 
 fn main() {
@@ -60,7 +76,7 @@ fn main() {
         process::exit(1);
     }
     let input_path = String::from(&args[1]);
-    let group_answers = match read_group_answers(input_path) {
+    let group_answers = match read_group_answers(&input_path) {
         Ok(answers) => answers,
         Err(e) => {
             eprintln!("Failed to read group answers: {}", e);
@@ -69,9 +85,13 @@ fn main() {
     };
 
     println!("\n==== Part 1 ====");
-    let total_answers = count_yes_answers(&group_answers);
+    let union_sum = sum_group_answers_union(&group_answers);
+    println!("Total \"yes\" answers, aggregated by union: {}", union_sum);
+
+    println!("\n==== Part 2 ====");
+    let inter_sum = sum_group_answers_inter(&group_answers);
     println!(
-        "Total \"yes\" answers, aggregated by group: {}",
-        total_answers
+        "Total \"yes\" answers, aggregated by intersection: {}",
+        inter_sum
     );
 }
