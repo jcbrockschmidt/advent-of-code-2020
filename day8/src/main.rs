6f02aa8d3952a8661cb070560a67626a123274ff
Jaclyn Brockschmidt <jcbrockschmidt@gmail.com>
2020-12-08 13:14:35 -0800

    Add solution for day 8, part 2
    Solved in 44:22

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -3,12 +3,14 @@ use std::path::Path;
 use std::process;
 use utils::read_lines;
 
+#[derive(Clone)]
 enum InstrType {
     ACC,
     JMP,
     NOP,
 }
 
+#[derive(Clone)]
 struct Instruction {
     pub typ: InstrType,
     pub val: i64,
@@ -72,31 +74,83 @@ where
     Ok(ins)
 }
 
-/// Gets the value of the accumulator as per the instruction set before any instructions repeat.
-fn get_acc_before_repeats(ins: &Vec<Instruction>) -> i64 {
+/// Executes the instructions, returning the accumulator value (before any loop),
+/// the instructions executed, and whether the program terminated (as opposed to
+/// getting stuck in a loop).
+fn execute(ins: &Vec<Instruction>) -> (i64, Vec<bool>, bool) {
     let mut visited = Vec::new();
     for _ in 0..ins.len() {
         visited.push(false);
     }
-    let mut i: i64 = 0;
+    let mut i: usize = 0;
     let mut acc: i64 = 0;
-    while i >= 0 && !visited[i as usize] {
+    while i < ins.len() && !visited[i] {
         visited[i as usize] = true;
-        let cur_in = &ins[i as usize];
+        let cur_in = &ins[i];
         match cur_in.typ {
             InstrType::ACC => {
                 acc += cur_in.val;
                 i += 1;
             }
             InstrType::JMP => {
-                i += cur_in.val;
+                if cur_in.val < 0 {
+                    if ((-cur_in.val) as usize) > i {
+                        // Cannot have a negative i.
+                        break;
+                    } else {
+                        i -= (-cur_in.val) as usize;
+                    }
+                } else {
+                    i += cur_in.val as usize;
+                }
             }
             InstrType::NOP => {
                 i += 1;
             }
         }
     }
-    acc
+    (acc, visited, i >= ins.len())
+}
+
+/// Gets the value of the accumulator as per the instruction set before any instructions repeat.
+fn get_acc_before_repeats(ins: &Vec<Instruction>) -> i64 {
+    let (acc_val, _, _) = execute(ins);
+    acc_val
+}
+
+/// Changes a jmp or nop instruction so the program terminates, returning the final accumulator value.
+fn repair_and_execute(ins: &Vec<Instruction>) -> Option<i64> {
+    let (acc_val, executed, term) = execute(ins);
+    if term {
+        return Some(acc_val);
+    }
+    // Copy the intructions so we can mutate them freely.
+    let mut ins: Vec<Instruction> = ins.iter().cloned().collect();
+
+    // Try substituting each nop and jmp and see if the program executes.
+    for (i, executed) in executed.iter().enumerate() {
+        if !executed {
+            continue;
+        }
+        let new_typ = match ins[i].typ {
+            InstrType::JMP => InstrType::NOP,
+            InstrType::NOP => InstrType::JMP,
+            _ => continue,
+        };
+        let old_in = ins.remove(i);
+        let new_in = Instruction {
+            typ: new_typ,
+            val: old_in.val,
+        };
+        ins.insert(i, new_in);
+        let (acc_val, _, term) = execute(&ins);
+        if term {
+            return Some(acc_val);
+        }
+        ins.remove(i);
+        ins.insert(i, old_in);
+    }
+    None
 }
 
 fn main() {
@@ -115,6 +169,12 @@ fn main() {
     };
 
     println!("\n==== Part 1 ====");
-    let acc_val = get_acc_before_repeats(&ins);
-    println!("Accumulator value before repeats: {}", acc_val);
+    let acc_val_no_repair = get_acc_before_repeats(&ins);
+    println!("Accumulator value before repeats: {}", acc_val_no_repair);
+
+    println!("\n==== Part 2 ====");
+    match repair_and_execute(&ins) {
+        Some(acc) => println!("Accumulator value after repairing: {}", acc),
+        None => println!("No possible repair found"),
+    }
 }
