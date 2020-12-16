687663547875454f789605413ddd325a7eda1970
Jaclyn Brockschmidt <jcbrockschmidt@gmail.com>
2020-12-15 22:59:51 -0800

    Add solution for day 15, part 1
    Solved in 37:14

---
+++ day15/src/main.rs
@@ -0,0 +1,60 @@
+#[macro_use]
+extern crate utils;
+
+use std::collections::HashMap;
+use std::env;
+use std::process;
+
+/// Prints usage statement for the executable.
+fn usage(args: Vec<String>) {
+    println!("Usage: {} starting_numbers", args[0]);
+}
+
+/// Returns the number spoken on the `last_turn`th turn.
+fn memory_game(start_nums: &Vec<usize>, last_turn: usize) -> usize {
+    let mut last_spoke: HashMap<usize, usize> = HashMap::new();
+    for (i, n) in start_nums.iter().enumerate() {
+        if i == start_nums.len() - 1 {
+            break;
+        }
+        // Turn numbers start at 1, not 0.
+        last_spoke.insert(*n, i + 1);
+    }
+    // Assumes there are some start numbers.
+    let mut prev = *start_nums.last().unwrap();
+    for turn in (start_nums.len() + 1)..(last_turn + 1) {
+        let cur: usize = if last_spoke.contains_key(&prev) {
+            turn - 1 - last_spoke.get(&prev).unwrap()
+        } else {
+            0
+        };
+        last_spoke.insert(prev, turn - 1);
+        prev = cur;
+    }
+    prev
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        usage(args);
+        process::exit(1);
+    }
+    let start_nums: Vec<usize> = {
+        let mut nums = Vec::new();
+        for num in args[1].split(',') {
+            match num.parse::<usize>() {
+                Ok(n) => nums.push(n),
+                Err(_) => {
+                    eprintln!("Failed to parse starting numbers");
+                    process::exit(1);
+                }
+            }
+        }
+        nums
+    };
+
+    timed_section!("Part 1", { memory_game(&start_nums, 2020) }, |v| {
+        println!("At turn 2020: {}", v);
+    });
+}
